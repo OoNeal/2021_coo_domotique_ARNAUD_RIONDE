52bcae91cf46bfa7708230f41f3f858a997374f6
toinou887 <rionde8u@etu.univ-lorraine.fr>
2022-04-07 09:04:57 +0200

    ajout des signatures des methodes

--- src/Telecommande.java
+++ src/Telecommande.java
@@ -1,2 +1,44 @@
+import java.util.ArrayList;
+
 public class Telecommande {
+
+    private ArrayList<Lampe> lampes;
+
+    public Telecommande()
+    {
+
+    }
+
+    public void ajouterLampe(Lampe l)
+    /*{
+        lampes.add(l);
+    } */
+    {
+        throw new Error("code non ecrit");
+    }
+
+    public void activerLampe(int indiceLampe)
+    /*{
+        lampes.get(indiceLampe).allumer();
+    } */
+    {
+        throw new Error("code non ecrit");
+    }
+
+    public void desactiverLampe(int indceLampe)
+    {
+        throw new Error("code non ecrit");
+    }
+
+    public void activerTout()
+    {
+        throw new Error("code non ecrit");
+    }
+
+    public String toString()
+    {
+        throw new Error("code non ecrit");
+    }
+
+
 }
