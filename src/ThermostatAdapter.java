bfcdfa59f788484673939e9dad22d5d3ecb35d72
toinou887 <rionde8u@etu.univ-lorraine.fr>
2022-05-02 09:01:54 +0200

    classe ThermostatAdapter terminé

--- src/ThermostatAdapter.java
+++ src/ThermostatAdapter.java
@@ -1,3 +1,16 @@
-public class ThermostatAdapter {
+import thermos.Thermostat;
 
+public class ThermostatAdapter extends Thermostat implements Appareil {
+
+    public ThermostatAdapter(){
+        super();
+    }
+
+    public void allumer() {
+        this.monterTemperature();
+    }
+
+    public void eteindre(){
+        this.baisserTemperature();
+    }
 }
